785e65d0fbb2f9b5c8b68bc153885b818c5a93ae
Collin-W <whalencollin@gmail.com>
2021-10-13 15:25:21 -0500

    user input validation

--- app.js
+++ app.js
@@ -6,17 +6,41 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!')
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username' ,
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!')
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      validate: aboutInput => {
+        if (aboutInput) {
+          return true;
+        } else {
+          console.log('Please enter some info about yourself.')
+          return false;
+        }
+      }
     }
   ])
 }
